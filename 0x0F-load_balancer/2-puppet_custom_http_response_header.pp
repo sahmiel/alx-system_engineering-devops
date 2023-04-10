62542581cf0d220e8a3f47368f7dc87942067385
Mogbolahan Folarin <sahmiel@outlook.com>
2023-04-10 15:52:14 -0700

    Advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,36 @@
 #!/usr/bin/env bash
-# Install nginx server
+
+set -e # Exit immediately if a command exits with a non-zero status
+
+# Install Nginx
 sudo apt-get update -y
 sudo apt-get install -y nginx
 
-# Config firewall
+# Configure firewall
 sudo ufw allow 'Nginx HTTP'
+
+# Create directory for web pages
 sudo mkdir -p /var/www/html
 
-# Config permissions
+# Set permissions for web directory
 sudo chmod -R 755 /var/www
 
-# Create index page
-echo 'Hello World!' |sudo tee  /var/www/html/index.html
+# Create custom index page
+echo 'Hello World!' | sudo tee /var/www/html/index.html
 
-# Create 404 page
-echo "Ceci n'est pas une page" |sudo tee /var/www/html/404.html
+# Create custom 404 error page
+echo "Ceci n'est pas une page" | sudo tee /var/www/html/404.html
 
-# Set redirect for a single page(/redirect_me)
+# Configure redirect for a single page (/redirect_me)
+redirect_config="server_name _;\n\tadd_header X-Served-By \$hostname;\n\trewrite ^\/redirect_me https:\/\/www.javalite.tech permanent;"
+sudo sed -i "s/server_name _;/$redirect_config/" /etc/nginx/sites-enabled/default
 
-string_for_replacement="server_name _;\n\tadd_header X-Served-By \$hostname;\n\trewrite ^\/redirect_me https:\/\/www.javalite.tech permanent;"
+# Configure redirect for 404 errors
+error_config="listen 80 default_server;\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}"
+sudo sed -i "s/listen 80 default_server;/$error_config/" /etc/nginx/sites-enabled/default
 
-sudo sed -i "s/server_name _;/$string_for_replacement/" /etc/nginx/sites-enabled/default
-
-# Config redirect for 404 error
-
-string_for_replacement="listen 80 default_server;\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}"
-
-sudo sed -i "s/listen 80 default_server;/$string_for_replacement/" /etc/nginx/sites-enabled/default
-
-# Restart web server
+# Restart Nginx service
 sudo service nginx restart
+
+echo "Nginx installed and configured successfully!"
+
